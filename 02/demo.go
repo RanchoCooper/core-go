c5649ecda9b2cb4ffc77259eeb64e344b3fee01b
Rancho <ranchocooper@gmail.com>
2020-08-23 20:48:06 +0800

    up

--- 02/demo.go
+++ 02/demo.go
@@ -8,16 +8,25 @@ import (
 
 var name string
 
+var cmdLine = flag.NewFlagSet("question", flag.ExitOnError)
+
 func init() {
+	flag.CommandLine = flag.NewFlagSet("", flag.PanicOnError)
+	flag.CommandLine.Usage = func() {
+		_, _ = fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+		flag.PrintDefaults()
+	}
 	flag.StringVar(&name, "name", "rancho", "the name of greeting object")
 }
 
 func main() {
-	flag.Usage = func() {
-		_, _ = fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
-		flag.PrintDefaults()
-	}
+	// flag.Usage = func() {
+	// 	_, _ = fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+	// 	flag.PrintDefaults()
+	// }
 
-	flag.Parse()
+	// flag.Parse()
+	cmdLine.Parse(os.Args[1:])
+
 	fmt.Printf("hello, %s\n", name)
 }
