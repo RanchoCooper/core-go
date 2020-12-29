4ec26052fdd49f17f627e8c1998f3d3b2d71c7e8
Rancho <ranchocooper@gmail.com>
2020-12-29 20:04:57 +0800

    up

--- 06/demo.go
+++ 06/demo.go
@@ -11,9 +11,9 @@ func main() {
 	if ok {
 		fmt.Printf("The element is %q.\n", value)
 	}
-	container := map[int]string{0: "zero", 1: "one", 2: "two"}
-	value, ok := interface{}(container).(map[int]string)
-	if ok {
-		fmt.Printf("The element is %q.\n", value)
-	}
+	// container := map[int]string{0: "zero", 1: "one", 2: "two"}
+	// // value, ok := interface{}(container).(map[int]string)
+	// // if ok {
+	// // 	fmt.Printf("The element is %q.\n", value)
+	// }
 }
