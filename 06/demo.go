ca8c3b9818a4eeae55eef417eb7ce7702e9f4b06
Rancho <ranchocooper@gmail.com>
2020-12-29 20:04:27 +0800

    up

---
+++ 06/demo.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+var container = []string{"zero", "one", "two"}
+
+func main() {
+	value, ok := interface{}(container).([]string)
+	if ok {
+		fmt.Printf("The element is %q.\n", value)
+	}
+	container := map[int]string{0: "zero", 1: "one", 2: "two"}
+	value, ok := interface{}(container).(map[int]string)
+	if ok {
+		fmt.Printf("The element is %q.\n", value)
+	}
+}
