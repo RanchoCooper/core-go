025b86355493da9a68bac28fae482e63445a4a11
Rancho <ranchocooper@gmail.com>
2021-01-03 21:46:25 +0800

    up

---
+++ 15/demo.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type Named interface {
+	Name() string
+}
+
+type Dog struct {
+	name string
+}
+
+func (dog *Dog) SetName(name string) {
+	dog.name = name
+}
+
+func (dog Dog) Name() string {
+	return dog.name
+}
+
+func New(name string) Dog {
+	return Dog{name}
+}
+
+func main() {
+	// 不可寻址 New("little pig").SetName("monster")
+	dog := New("little pig")
+	// dog.SetName("monster")
+
+	dogP := &dog
+	dogPtr := uintptr(unsafe.Pointer(dogP))
+	namePtr := dogPtr + unsafe.Offsetof(dogP.name)
+	nameP := (*string)(unsafe.Pointer(namePtr))
+
+	fmt.Printf("nameP == &(dogP.name)? %v\n",
+		nameP == &(dogP.name))
+	fmt.Printf("The name of dog is %q.\n", *nameP)
+
+	*nameP = "monster"
+	fmt.Printf("The name of dog is %q.\n", dogP.name)
+	fmt.Println()
+
+	// 下面这种不匹配的转换虽然不会引发panic，但是其结果往往不符合预期。
+	numP := (*int)(unsafe.Pointer(namePtr))
+	num := *numP
+	fmt.Printf("This is an unexpected number: %d\n", num)
+}
